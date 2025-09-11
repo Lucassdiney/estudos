c826d4985dca0c7f5bae70e45e5aedbc89c4b36f
Lucas Sidney <lucassidney1999@gmail.com>
2025-09-10 22:07:43 -0300

    começando na linguagem C

---
+++ exercicio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*inicio dos meus estudos na linguagem C
+    relembrando o que já havia praticado anteriormente*/
+
+    int F;
+    F = 123;
+    printf("o número inteiro é: %d\n",F);
+
+    float G;
+    G = 123.12;
+    printf("O número com vírgula é: %.3f", (float)G);
+}
