f76219eb8a40aef11b7ff60fb214716dbf11a5a6
Lucas Sidney <lucassidney1999@gmail.com>
2025-09-26 10:55:46 -0300

    treinando C

---
+++ exercicio02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int minhaIdade;
+    minhaIdade = 26;
+
+    int maeIdade = 48;
+    int paiIdade = 52;
+
+    int idadeTotal = minhaIdade + maeIdade + paiIdade;
+
+    printf("minha idade é: %i\n", minhaIdade);
+    printf("idade de meu pai é: %i\n",paiIdade);
+    printf("idade de minha mãe é: %i\n", maeIdade);
+
+    printf("idade total calculada é: %i\n", idadeTotal);
+
+    /*Declarando variaveis e operações matematica*/
+}
---
+++ exercicio03.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    float base;
+    float altura;
+    float area;
+
+    printf("Qual o valor da base: \n");
+    scanf("%f", &base);
+
+    printf("qual o valor da altura: \n");
+    scanf("%f", &altura);
+
+    area = base * altura;
+
+    printf("O valor da area é: %.2f \n", area);
+
+    /*Calculando area de um retangulo e 
+    testando novas variaveis.*/
+}
